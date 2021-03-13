ac516d062ca23afe6b35e8b11282ad2950be303c
junron <junron1@outlook.com>
2021-03-13 13:00:25 +0000

    Remove verify script for mission 2

--- verify2-1.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/python3
-
-import subprocess, sys
-stri = subprocess.check_output(["git", "status"]).decode("utf-8")
-if "modified:   file1.txt" in stri and "new file:   file2.txt" in stri:
-    sys.stdout.write("Congratulations, you have completed task 2-1!\n")
-else:
-    sys.stdout.write("Task incomplete, please try again.\n")
